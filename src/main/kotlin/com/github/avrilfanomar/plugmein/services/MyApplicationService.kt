578b6c4edbda7dbac476e86375fea67b2406b54b
GitHub Action <action@github.com>
2021-10-21 19:13:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/avrilfanomar/plugmein/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/avrilfanomar/plugmein/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.avrilfanomar.plugmein.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.avrilfanomar.plugmein.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/avrilfanomar/plugmein/services/MyProjectService.kt
+++ src/main/kotlin/com/github/avrilfanomar/plugmein/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.avrilfanomar.plugmein.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.avrilfanomar.plugmein.MyBundle
 
 class MyProjectService(project: Project) {
 
